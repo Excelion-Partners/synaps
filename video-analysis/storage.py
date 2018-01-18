21cd645d1f89347e3cca9d3edda38f95ef1f2a06
Ryan Moore <rmoore@excelionit.com>
2018-01-18 11:25:42 -0600

    archive

--- video-analysis/storage.py
+++ video-analysis/storage.py
@@ -2,6 +2,9 @@ import sys
 import os
 import time
 import datetime
+import json
+
+import psycopg2
 
 from face_session import FaceSession
 from logger import Logger
@@ -9,11 +12,22 @@ from logger import Logger
 class Storage:
     
     def __init__(self, descriptor):
-        
-    
+        self.conn_str = "dbname=synaps user=synaps password=3wPUBimpTH6Y"
+
+    def get_changes(self):
+        conn = psycopg2.connect(self.conn_str)
+        cur = conn.cursor()
+        cur.execute("""
+          SELECT
+             row_to_json(sessions)
+             FROM sessions
+             LIMIT 10
+        """)
+
+
     def insert_session(self, face, session):
         try:
-            conn = psycopg2.connect("dbname=synaps user=synaps password=3wPUBimpTH6Y")
+            conn = psycopg2.connect(self.conn_str)
             cur = conn.cursor()
 
             sql = """INSERT INTO sessions(start_date, duration, age, sex) values (%s, %s, %s, %s);"""
