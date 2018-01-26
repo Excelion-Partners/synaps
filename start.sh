026434d502caabddc2c7ba003fc5ea2250f85a5f
Ryan Moore <rmoore@excelionit.com>
2018-01-26 10:21:10 -0600

    d

--- start.sh
+++ start.sh
@@ -39,7 +39,7 @@ rm /tmp/.X0-lock &>/dev/null || true
 # dashboard
 cd /usr/app/dashboard/
 
-yarn start & sleep 5s && cd /usr/app/socket/ && node index.js & sleep 30s && cd /usr/app/video-analysis && python3 main.py & startx /usr/app/electron/node_modules/electron/dist/electron /usr/app/electron --enable-logging 
+yarn start & sleep 15s && startx /usr/app/electron/node_modules/electron/dist/electron /usr/app/electron --enable-logging & cd /usr/app/socket/ && node index.js & sleep 1s && cd /usr/app/video-analysis && python3 main.py 
 
 while true
 do
